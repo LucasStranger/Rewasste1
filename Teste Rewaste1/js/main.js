ca502efe5e3c1dc88cdbc488d3e3d2678b686976
Valmir Klenio <valmirklenio@hotmail.com>
2024-10-01 19:36:59 -0300

    update index e js

--- Teste Rewaste1/js/main.js
+++ Teste Rewaste1/js/main.js
@@ -12,3 +12,22 @@ menuIcon.addEventListener('click',()=>{
     }
 
 })
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); 
+
+            const targetID = this.getAttribute("href");
+            const targetSection = document.querySelector(targetID);
+
+            const topOffset = targetSection.offsetTop;
+
+            window.scrollTo({
+                top: topOffset,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
